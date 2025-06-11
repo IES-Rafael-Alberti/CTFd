e9c3df2f88d300abcea4f77daa42aae06788f8cb
Ángel Pereira <ampr2003@gmail.com>
2025-06-11 20:42:15 +0200

    CTFd GitHub integration configuration v13

--- CTFd/themes/admin/assets/js/pages/configs.js
+++ CTFd/themes/admin/assets/js/pages/configs.js
@@ -746,7 +746,7 @@ $(() => {
           nameTd.textContent = repo.full_name;
 
           const lastImportTd = document.createElement("td");
-          lastImportTd.textContent = repo.last_synced_at ? `${repo.last_synced_at} UTC` : "-";
+          lastImportTd.textContent = repo.last_synced_at ? `${repo.last_synced_at.isoformat(sep = " ", timespec = "seconds")} UTC` : "-";
 
           const actionsTd = document.createElement("td");
           actionsTd.innerHTML = `
