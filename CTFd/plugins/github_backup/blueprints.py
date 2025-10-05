1b52dc1273fbdbb9ea78cf8152eabdf687a34a24
avilrod3004 <avilrod3004@g.educaand.es>
2025-10-05 14:07:50 +0200

    Refactor and enhance "github_backup" plugin: add admin-only access to routes, replace hardcoded GitHub credentials with config-based values, update imports functionality with overwrite and delete options, and implement example JSON download for challenges.

--- CTFd/plugins/github_backup/blueprints.py
+++ CTFd/plugins/github_backup/blueprints.py
@@ -5,6 +5,8 @@ from flask import Blueprint, render_template, jsonify, Response
 from CTFd.plugins import bypass_csrf_protection
 from CTFd.plugins.github_backup.expot_data import is_imported_from_github, prepare_json
 from CTFd.plugins.github_backup.import_data import import_challenges_from_repo
+from CTFd.utils import get_app_config
+from CTFd.utils.decorators import admins_only
 
 # Definimos un Blueprint propio.
 # Elegimos un nombre (interno) y un prefijo de URL.
@@ -21,13 +23,14 @@ my_bp = Blueprint(
 )
 
 @my_bp.route("/admin/plugins/github_backup")
+@admins_only
 def vista_panel_admin():
     """
     Esta función renderiza la nueva página que aparecerá
     en el menú 'Plugins → Mi Plugin' (ruta: /admin/plugins/github_backup).
     """
-    total = Challenges.query.count()
-    return render_template("admin/challenges/github_backup.html", total_challenges=total)
+    installation_url = config["GITHUB_APP_INSTALLATION_URL"]
+    return render_template("admin/challenges/github_backup.html", installation_url=installation_url)
 
 
 @my_bp.route("/api/v1/github_backup/challenges", methods=["GET"])
@@ -70,6 +73,7 @@ import jwt
 
 import io
 import zipfile
+from CTFd.plugins.github_backup.config import config
 
 # github_namespace = Namespace(
 #     'github', description='Endpoint to manage challenge sync from github'
@@ -79,10 +83,8 @@ def generate_jwt():
     """
     Generates a JWT for the GitHub App.
     """
-    # app_id = get_app_config("GITHUB_APP_ID")
-    # private_key = open(get_app_config("GITHUB_APP_PRIVATE_KEY_PATH"), "r").read()
-    app_id = "1304835"
-    private_key = "-----BEGIN RSA PRIVATE KEY-----\nMIIEpQIBAAKCAQEAySt1/kZawxWG72EgRlDWSPOy2KJvfoWpiZQA+4qKhMqyS+WZ\nHjJ9RXZ3fHdpXjp4vYMb5JPTsGcQUS8dPK97vDw4HELfEtAsKI9WO817e3RZh6Mm\nCELoDcvvHJhPOnYiRQmmHXBLqcgYhzXLn3OQs1iAthVCdzA+QeeHdvesoDM6+jGJ\nRiAUvpIoRnaJw1xUv15fBm/r5qnO9VYndEKCNVpcrJsfAx2YsBlV+Qofn/3zXXRn\nkcHwYXCWqKHi7gVQ4+Yl4UwNbM7EYu9R+9jwdD5QdDOa33LySs/k1c4iiuZRfyea\nSJ6GAYBwbr3z+BiFjRtOupyU1SqKYE4VdU+WmwIDAQABAoIBAFiB+J3xEkm2lPGJ\nCAwn0L+0CMHrZaaC5D6VkYEaSSt3is59q8pvaFJBdvyaBQ/iJfErWABPUdnQ3or5\ncaGbi5Mb44v7l+3NDUCjH8ub8nI0lJdqnncQCMzZJtCr6l/MTfOV9TIKkB+Y28HR\nV09GJynhDHAQFngeD6YrWZxsB9VLLKKDilTJnrr9lgpoqXyr+wTANonUtkABo+N8\neLEYvC7RDQNgXFM6gWf3vB5VScI5iwj3i/snrWOCq6i93BZvK/G80V2WzfLU7+Uk\nHOHy6Cb7XlnMe20UIhy2R4agU71tOv1TEkNkTAU2bofGnhF95SDRLutEHS5RzAhK\nCwPoyIECgYEA9UZVWLpWt4Fk8S1fGbvkjjUI6mhAoOOqxNTKdxH2gL6g6KQuoWPr\nnhplyogeBWzlnaOp6M/11n9iiXNDS++1qkf2u+F9fXh2amXhOD0NjYUBhCyHx5kG\nj/qqAbGGcsx4eN6mfrgiXjElJ/AKqSfU+Fcf5uwRzqp1QtR+I/9zFqECgYEA0fdn\nzdCzgoXaOn1XHU5bmSW/jw6y1bQsupEMtVwWTDlUApSwwCR8/unJmQ9alZ0gkBZ+\ngVxtOLvcBTAHagxdBLHE8p3XTYL+2ciZ8/nbAvW9oSx+8WUBKGaNuh2NLvSz+19D\n51HaCoByZ6N266LTEw4vrxQjvr2aAHuL3qgGr7sCgYEA4/3f4Z08dFFj+ke5ids9\nBe9IfVQxFxzppRNDrFDXOS95tSbAbabK4ukYy52hgluKSPFURCPjGeLJPokU+iY1\nULaaJGTjWFk8gYR4SOV/jpgfI7Sc9i1Ji1Zu5yoUlut35KraSxpwX+wA5NHdpwVu\nkvWxot63YsrSOy/4nQ+b9GECgYEAqOJhYlDeMrxLXgR3T6E5WCPewsjF29LCr4YA\nPNhHLyRliHXk/Yfb6oZNm4mYCQOLr5oIQzD+wUWJobqSHCC5qQ78JVIEXdph9va9\nrlpoaScUUlGiAbSGi+KBWRRv/Tbilex4h9V0lkHew7Hd84bSVF9yx1Zhsgh7mKvH\nQ17PkgUCgYEAy2Bcdey9BBFgnR7daX1FoZokb2fEJp/xEwnXNzWyGcunVE2nTIHl\nhXaei0zYvwhCG76GZnp/EJHCvtfFYCSc26X1VwpPUwA3nS+5UDjvoo5ouT/5Rhvv\nDrR0oMwNmmAGmhIcDPH961etE0GydOAnjyYat2fw8P1OZRUUENYb2Dg=\n-----END RSA PRIVATE KEY-----"
+    app_id = config["GITHUB_APP_ID"]
+    private_key = config["GITHUB_APP_PRIVATE_KEY"]
 
     payload = {
         "iat": int(time.time()),
@@ -179,7 +181,6 @@ def link_installation():
     return {"success": True, "message": f"Installation ID {installation_id} saved correctly."}
 
 # lista los repositorios de la cuenta de usuario
-# @github_namespace.route('/repos')
 @my_bp.route("/plugins/github_backup/repos", methods=["GET"])
 @admins_only
 def get_repos():
@@ -266,10 +267,10 @@ def save_selected_repos():
     return {"success": True, "message": "Repositories saved correctly"}
 
 
-# List the saved challenges
+# List the saved repos
 @my_bp.route("/plugins/github_backup/repos/saved", methods=["GET"])
 @admins_only
-def list_saved_challenges():
+def list_saved_repos():
     user_id = get_current_user().id
     saved_repos = GithubRepositories.query.filter_by(user_id=user_id).all()
 
@@ -321,6 +322,9 @@ def delete_repo(repo_id):
 @my_bp.route("/plugins/github_backup/repos/<int:repo_id>/import", methods=["POST"])
 @admins_only
 def import_from_repo(repo_id):
+    data = request.get_json()
+    delete_mode = data.get("delete_mode")
+
     try:
         user_id = get_current_user().id
         repo = GithubRepositories.query.filter_by(id=repo_id, user_id=user_id).first()
@@ -333,7 +337,7 @@ def import_from_repo(repo_id):
             return {"success": False, "message": "No GitHub token configured"}, 400
         access_token = get_installation_access_token(token_entry.token)
 
-        result = import_challenges_from_repo(repo, access_token, overwrite_existing=False)
+        result = import_challenges_from_repo(repo, access_token, overwrite_existing=True, delete_mode=delete_mode)
 
         repo.selected = True
         repo.last_synced_at = datetime.now().astimezone(pytz.utc)
@@ -341,11 +345,11 @@ def import_from_repo(repo_id):
 
         return {
             "success": result["success"],
-            "message": f"{result['created']} challenges imported, {result['skipped']} already existing.",
+            "message": f"{result['created']} challenges imported, {result['updated']} challenges updated, {result['skipped']} already existing, {result['removed']} deleted",
             "errors": result["errors"]
         }
     except Exception as e:
-        return {"success": False, "message": f"Unexpected error: {str(e)}"}, 500
+        return {"success": False, "message": e}, 500
 
 
 @my_bp.route("/plugins/github_backup/challenge/<int:challenge_id>/download", methods=["GET"])
@@ -386,6 +390,68 @@ def download_challenge(challenge_id: int) -> tuple[dict[str, bool | str], int] |
         return {"success": False, "message": str(e)}, 400
     except Exception as e:
         return {"success": False, "message": f"Unexpected error: {str(e)}"}, 500
+
+@my_bp.route("/plugins/github_backup/challenges/download/example", methods=["GET"])
+@admins_only
+def download_example_json():
+    example = {
+        "challenge": {
+            "uuid": "000000000000000",
+            "name": "knock, knock, Neo",
+            "description": "Wake up. The matrix has you...",
+            "attribution": "author",
+            "connection_info": "https://link.com",
+            "max_attempts": 3,
+            "value": 50,
+            "category": "web",
+            "type": "standard",
+            "state": "visibe or hidden",
+            "flags": [
+                {
+                    "uuid": "000000000000000",
+                    "type": "static",
+                    "content": "flag{answer}",
+                    "data": "case_insensitive",
+                },
+                {
+                    "uuid": "000000000000000",
+                    "type": "regex",
+                    "content": "flag{.a*}",
+                    "data": "",
+                }
+            ],
+            "tags": [
+                "tag1", "tag2"
+            ],
+            "hints": [
+                {
+                    "uuid": "000000000000000",
+                    "title": "Hint 1",
+                    "type": "standard",
+                    "content": "Follow the white rabbit...",
+                    "cost": 10
+                },
+                {
+                    "uuid": "000000000000000",
+                    "title": "Hint 2",
+                    "type": "?",
+                    "content": "?",
+                    "cost": 20
+                }
+            ],
+        }
+    }
+
+    json_bytes = json.dumps(example, indent=4, ensure_ascii=False).encode("utf-8-sig")
+
+    return Response(
+        json_bytes,
+        mimetype="application/json",
+        headers={
+            "Content-Disposition": f'attachment; filename="challenge_example.json"'
+        },
+    )
+
 
 @my_bp.route("/plugins/github_backup/challenges", methods=["GET"])
 @admins_only
